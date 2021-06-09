0fa38d7773bf3c1692c75d796e3474829063eb6a
Rakshanda Jha <rxj7063@Rj-MacBook-Pro.local>
2021-06-09 18:27:36 -0400

    Added three new packages within the fhire package

---
+++ src/main/java/io/connectedhealth_idaas/eventbuilder/pojos/clinical/fhir/ServiceRequestResource/Collection.java
@@ -0,0 +1,19 @@
+package io.connectedhealth_idaas.eventbuilder.pojos.clinical.fhir.ServiceRequestResource;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+import java.util.Date;
+
+public class Collection {
+    public Date collectedDateTime;
+
+    public Date getCollectedDateTime() { return collectedDateTime; }
+
+    public void setCollectedDateTime(Date collectedDateTime) { this.collectedDateTime = collectedDateTime; }
+
+    public String toString()
+    {
+        return ReflectionToStringBuilder.toString(this);
+    }
+
+}
